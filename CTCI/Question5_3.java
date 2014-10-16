19b59a4e01889d4b0559ad82af1c321d687ad6f2
ycczkl <ycczkl91@gmail.com>
2014-10-15 21:10:18 -0400

    add q5_3, 5_4

---
+++ CTCI/Question5_3.java
@@ -0,0 +1,55 @@
+public class Question5_3 {
+	public static int getNext(int n) {
+		int c = n;
+		int c0 = 0;
+		int c1 = 0;
+		while ((c & 1) == 0 && c != 0) {
+			c0++;
+			c >>= 1;
+		}
+		
+		while ((c & 1) == 1) {
+			c1++;
+			c >>= 1;
+		}
+		
+		if (c0 + c1 == 31 || c0 + c1 == 0) {
+			return -1;
+		}
+		
+		int p = c0 + c1;
+		
+		n &= ~((1 << p) - 1);
+		n |= (1 << (c1 - 1)) - 1;
+		
+		return n;
+	}
+	
+	public static int getPrev(int n) {
+		int temp = n;
+		int c0 = 0;
+		int c1 = 0;
+		
+		while ((temp & 1) == 1) {
+			c1++;
+			temp >>= 1;
+		}
+		
+		if (temp == 0) {
+			return -1;
+		}
+		
+		while (((temp & 0) == 0) && temp != 0) {
+			c0++;
+			temp >>= 1;
+		}
+		
+		int p = c0 + c1;
+		
+		n &= ((~0) << (p + 1));
+		int mask = (1 << (c1 + 1)) - 1;
+		n |= mask << (c0 - 1);
+		
+		return n;
+	}
+}
---
+++ CTCI/Question5_4.java
@@ -0,0 +1,4 @@
+
+public class Question5_4 {
+
+}
